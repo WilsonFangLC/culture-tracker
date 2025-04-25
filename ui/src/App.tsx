6d536523bbe00e2c19f7218b521eac5f1316af0c
WilsonFangLC <fanglc.wilson@icloud.com>
2025-04-25 14:25:42 +0800

    Remove CreateStateForm and Login components, along with the associated database file.

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,4 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
-import Login from './pages/Login'
 import States from './pages/States'
 
 function App() {
@@ -7,7 +6,6 @@ function App() {
     <div className="min-h-screen bg-gray-100">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <Routes>
-          <Route path="/login" element={<Login />} />
           <Route path="/states/" element={<States />} />
           <Route path="/" element={<Navigate to="/states/" replace />} />
         </Routes>
